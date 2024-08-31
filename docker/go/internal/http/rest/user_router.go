5ee862615eb055ed5b8c0a31d25aca2d2e5a8273
rm-ryou <emco.420118@gmail.com>
2024-08-31 15:31:40 +0900

    add get user handler

--- docker/go/internal/http/rest/user_router.go
+++ docker/go/internal/http/rest/user_router.go
@@ -1,20 +1,42 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rm-ryou/sampleTodoApp/internal/service"
 )
 
+type GetUserRequest struct {
+	ID int `uri:"id" binding:"required"`
+}
+
 func BindUserRoutes(r *gin.Engine, us service.UserServicer) {
 	userRouter := r.Group("/api/v1/users")
 
+	getUser(userRouter, us)
 	getUsers(userRouter, us)
+}
+
+func getUser(r *gin.RouterGroup, us service.UserServicer) {
+	getUserHandler := func(c *gin.Context) {
+		var req GetUserRequest
+
+		if err := c.ShouldBindUri(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+
+		user := us.GetUser(req.ID)
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	}
+	r.GET("/:id", getUserHandler)
 }
 
 func getUsers(r *gin.RouterGroup, us service.UserServicer) {
 	getUsersHandler := func(c *gin.Context) {
 		users := us.GetUsers()
-		c.JSON(200, gin.H{"data": users})
+		c.JSON(http.StatusOK, gin.H{"data": users})
 	}
 	r.GET("", getUsersHandler)
 }
--- docker/go/internal/http/rest/user_router_test.go
+++ docker/go/internal/http/rest/user_router_test.go
@@ -12,8 +12,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type UserResBody struct {
-	Data []entity.User `json:"data"`
+type UserResponser interface {
+	[]entity.User | entity.User
+}
+
+type UserResponse[T UserResponser] struct {
+	Data T `json:"data"`
+}
+
+func TestGetUser(t *testing.T) {
+
+	t.Run("parameter is number", func(t *testing.T) {
+		expectedUser := testdata.UserTestData[1]
+		url := fmt.Sprintf("%s/api/v1/users/%d", baseURL, expectedUser.ID)
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, url, nil)
+
+		router.ServeHTTP(res, req)
+
+		var userResponse UserResponse[entity.User]
+		if err := json.Unmarshal(res.Body.Bytes(), &userResponse); err != nil {
+			t.Error(err)
+		}
+		user := userResponse.Data
+
+		assert.Equal(t, expectedUser.ID, user.ID)
+		assert.Equal(t, http.StatusOK, res.Code)
+	})
+
+	t.Run("parameter is not number", func(t *testing.T) {
+		url := fmt.Sprintf("%s/api/v1/users/hoge", baseURL)
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, url, nil)
+
+		router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+	})
 }
 
 func TestGetUsers(t *testing.T) {
@@ -23,11 +58,11 @@ func TestGetUsers(t *testing.T) {
 
 	router.ServeHTTP(res, req)
 
-	var userResBody UserResBody
-	if err := json.Unmarshal(res.Body.Bytes(), &userResBody); err != nil {
+	var userResponse UserResponse[[]entity.User]
+	if err := json.Unmarshal(res.Body.Bytes(), &userResponse); err != nil {
 		t.Error(err)
 	}
-	users := userResBody.Data
+	users := userResponse.Data
 
 	assert.Equal(t, 1, len(users))
 	assert.Equal(t, testdata.UserTestData[1].ID, users[0].ID)
