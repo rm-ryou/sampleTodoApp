1b4bf45ee831c52193d452498148492a6351198c
rm-ryou <emco.420118@gmail.com>
2024-08-31 18:45:42 +0900

    fix response data

--- docker/go/test/mock/service/user_service_mock.go
+++ docker/go/test/mock/service/user_service_mock.go
@@ -11,29 +11,26 @@ func NewUserServiceMock() *userServiceMock {
 	return &userServiceMock{}
 }
 
-func (usm *userServiceMock) CreateUser(name, email, password string) (*entity.User, error) {
-	return &testdata.UserTestData[1], nil
+func (usm *userServiceMock) CreateUser(name, email, password string) (*entity.UserResponse, error) {
+	return &testdata.UserResponseTestData[1], nil
 }
 
-func (usm *userServiceMock) GetUser(id int) (*entity.User, error) {
-	return &testdata.UserTestData[1], nil
+func (usm *userServiceMock) GetUser(id int) (*entity.UserResponse, error) {
+	return &testdata.UserResponseTestData[1], nil
 }
 
-func (usm *userServiceMock) GetUsers() ([]entity.User, error) {
-	return testdata.UserTestData[1:], nil
+func (usm *userServiceMock) GetUsers() ([]entity.UserResponse, error) {
+	return testdata.UserResponseTestData[1:], nil
 }
 
-func (usm *userServiceMock) EditUser(id int, name, email, password string) (*entity.User, error) {
-	baseUser := testdata.UserTestData[1]
+func (usm *userServiceMock) EditUser(id int, name, email, password string) (*entity.UserResponse, error) {
+	baseUser := testdata.UserResponseTestData[1]
 
 	if name != "" {
 		baseUser.Name = name
 	}
 	if email != "" {
 		baseUser.Email = email
-	}
-	if password != "" {
-		baseUser.Password = password
 	}
 
 	return &baseUser, nil
